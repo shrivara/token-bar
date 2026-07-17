8f6cd65fb7f19115ebe1209e9a7e0089276c7062
Shrivara <shrivara@users.noreply.github.com>
2026-07-17 01:53:03 -0700

    Bump version to 0.7.0

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ cat > "$APP/Contents/Info.plist" <<'EOF'
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>0.6.2</string>
+	<string>0.7.0</string>
 	<key>LSUIElement</key>
 	<true/>
 	<key>NSHighResolutionCapable</key>
