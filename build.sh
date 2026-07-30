915dc6b867d0b3797fa41be0812aa05c1f9fb9d6
Shrivara <shrivara@users.noreply.github.com>
2026-07-30 02:41:24 -0700

    Bump version to 0.8.0

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ cat > "$APP/Contents/Info.plist" <<'EOF'
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>0.7.8</string>
+	<string>0.8.0</string>
 	<key>LSUIElement</key>
 	<true/>
 	<key>NSHighResolutionCapable</key>
